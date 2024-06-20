c683b17b0e88410bb7540c1cf85e2f956ad5e8d7
liliValGo <lili.johanna.valgo.gmail.com>
2024-06-19 21:21:32 -0500

    feat:vehicle data

--- jobs/main.py
+++ jobs/main.py
@@ -1,7 +1,10 @@
 import os
+import random
 from confluent_kafka import SerializingProducer
 import simplejson as json
 from datetime import datetime
+from datetime import timedelta
+import uuid
 
 LONDON_COORDINATES = {"latitude": 51.5074, "longitude": -0.1278}
 BIRMINGHAM_COORDINATES = {"latitude": 52.4862, "longitude": -1.8904}
@@ -22,8 +25,49 @@ EMERGENCY_TOPIC = os.getenv('EMERGENCY_TOPIC', 'emergency_data')
 start_time = datetime.now()
 start_location = LONDON_COORDINATES.copy()
 
-# aqui empieza minuto 38
+def get_next_time():
+    global start_time
+    start_time += timedelta(seconds=random.randint(30, 60)) #update frecuency
+    return start_time
 
+        
+    
+def simulate_vehicle_movement():
+    global start_location
+    
+    # move towards Birmingham
+    start_location['latitude'] += LATITUDE_INCREMENT
+    start_location['longitude'] += LONGITUD_INCREMENT
+    
+    # Add some randomness to simulate actual road travel
+    start_location['latitude'] += random.uniform(-0.0005, 0.0005)
+    start_location['longitude'] += random.uniform(-0.0005, 0.0005)
+    
+    return start_location
+
+def generate_vehicle_data(device_id):
+    location = simulate_vehicle_movement()
+    return {
+        'id': uuid.uuid4(), #Generate a unique identification (UUID)
+        'deviceId': device_id,
+        'timestamp': get_next_time().isoformat(),
+        'location':(location['latitude'], location['longitude']),
+        'speed': random.uniform(10, 40),
+        'direction': 'North-East',
+        'make': 'BMW',
+        'model': 'C500',
+        'year': 2024,
+        'fuelType': 'Hibrid'   
+    }
+    
+def simulate_journey(producer, device_id):
+    while True:
+        vehicle_data = generate_vehicle_data(device_id)
+        
+      
+        break
+        
+        
 if __name__ == "__main__":
     producer_config = {
         'bootstrap.servers': KAFKA_BOOTSRAP_SERVERS,
