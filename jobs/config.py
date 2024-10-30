faf676be78cdb6561993f5ad4941fae8389f7246
liliValGo <lili.johanna.valgo.gmail.com>
2024-10-30 17:19:07 -0500

    Update .gitignore

--- jobs/config.py
+++ jobs/config.py
@@ -1,4 +1,4 @@
 configuration =  {
-    "AWS_ACCESS_KEY": "Your AWS access key",
-  "AWS_SECRET_KEY": "Your AWS secret key"
+    "AWS_ACCESS_KEY": "AKIA6GBMF2JBV4UJOMF2",
+  "AWS_SECRET_KEY": "OI1sXKIduR2DYu4/o0ImehfoYsLZpOPWiEzfFEJS"
   }
